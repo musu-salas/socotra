2f4f5e07cf5147830b2e0e356221afaf40da1a01
Nik Sumeiko <nik.sumeiko@gmail.com>
2017-12-05 22:38:08 +0100

    Translates client validations for page with location map

--- public/socotra.group-manage.location.map.js
+++ public/socotra.group-manage.location.map.js
@@ -91,7 +91,11 @@
 
     if (mapLocation.latlng !== str) {
       $('input[name="latlng"]').val(str);
-      setMessage('Your marker exact position is accepted', 'Hit <a id="save-button-trigger" href="./">"Save"</a> to save this position.', 'info');
+      setMessage(
+        __('Your marker exact position is accepted'),
+        __('Hit :link to save this position.', { button: '<a id="save-button-trigger" href="./">"' . __('Save') . '"</a>' }),
+        'info'
+      );
     }
   }
 
@@ -158,22 +162,38 @@
     if (!mapLocation.latlng) {
       getAddressLatLng(mapLocation.address_line_1 + ', ' + mapLocation.zip + ' ' + mapLocation.city + ', ' + mapLocation.country.name, function(latlng) {
         addMarker(latlng);
-        setMessage('Marker was set automatically', 'If it doesn\'t match exact position of your location, feel free to drag it.', 'warning');
+        setMessage(
+          __('Marker was set automatically'),
+          __('If it doesn\'t match exact position of your location, feel free to drag it.'),
+          'warning'
+        );
 
       }, function() {
         getAddressLatLng(mapLocation.city + ', ' + mapLocation.country.name, function(latlng) {
           addMarker(latlng);
-          setMessage('Automatic marker position is not accurate', 'Please drag the marker on your own to the desired location.', 'warning');
+          setMessage(
+            __('Automatic marker position is not accurate'),
+            __('Please drag the marker on your own to the desired location.'),
+            'warning'
+          );
 
         }, function() {
           getAddressLatLng(mapLocation.country.name, function(latlng) {
             addMarker(latlng);
-            setMessage('Automatic marker position is not accurate', 'Please drag the marker on your own to the desired location.', 'warning');
+            setMessage(
+              __('Automatic marker position is not accurate'),
+              __('Please drag the marker on your own to the desired location.'),
+              'warning'
+            );
 
           }, function() {
             $.get('http://ipinfo.io/geo', function(geo) {
               addMarker(geo.loc);
-              setMessage('It was not possible to set the marker automatically', 'Please drag the marker on your own to the desired location.', 'warning');
+              setMessage(
+                __('It was not possible to set the marker automatically'),
+                __('Please drag the marker on your own to the desired location.'),
+                'warning'
+              );
             }, 'jsonp');
           });
         });
@@ -185,7 +205,13 @@
     } else {
       centerMap(buildLatLng(mapLocation.latlng));
       enableMap(false);
-      setMessage('People shall be able to find your class location effortless', 'Please <a href="../" title="">add detailed address of the location</a> of your class before reviewing it on the map.', 'error');
+      setMessage(
+        __('People shall be able to find your class location effortless'),
+        __('Please :link of your class before reviewing it on the map.', {
+          link: '<a href="../" title="">' . __('add detailed address of the location') .'</a>'
+        }),
+        'error'
+      );
     }
   }
 
